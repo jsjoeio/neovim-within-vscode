c1f1ef17bc7515bbce78eb3f19720e26a00fdd65
Joe Previte <jjprevite@gmail.com>
2022-11-29 17:24:35 +0000

    chore: add global footer

---
+++ global-bottom.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="text-slate-400">
+    <span class="absolute bottom-0 left-2 p-4 text-xs" style="font-size: 10px;">Neovim Conf 2022</span>
+    <a class="absolute bottom-0 right-2 p-4 text-xs" href="https://twitter.com/jsjoeio"
+      style="font-size: 10px;">@jsjoeio</a>
+  </footer>
+</template>
+
